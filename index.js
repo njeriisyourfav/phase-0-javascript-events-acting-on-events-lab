5828bf3e6d0e8bdf5dc5d30424f4e9bc3096b3a2
lilian njeri ndungu <lilian.ndungu@student.moringaschool.com>
2022-08-17 20:52:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+function moveDodgerLeft(){
+const leftNumbers = dodger.style.left.replace("px", "");
+const left = parseInt(leftNumbers, 10);
+if (left > 0) {
+  dodger.style.left = `${left - 1}px`;
+ }
+}
+
+function moveDodgerRight() {
+const leftNumbers = dodger.style.left.replace("px", "");
+const left = parseInt(leftNumbers, 10);
+if (left < 200) {
+  dodger.style.left = `${left + 1}px`;
+}
+}
+document.addEventListener("keydown", function (event) {
+console.log(event);
+if (event.key === "ArrowLeft") {
+  moveDodgerLeft();
+}
+else if (event.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+});
